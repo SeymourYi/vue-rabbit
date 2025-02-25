6ba0e2c0539ddc36e67dab0a8057829efae9a766
百溪 <2963637346@qq.com>
2025-02-25 22:59:03 +0800

    插件引入懒加载优化

--- src/main.js
+++ src/main.js
@@ -1,31 +1,17 @@
 // import './assets/main.css'
 import '@/styles/common.scss'
-
+import { lazyPlugin } from "@/directives/index";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
 
-app.directive('img-lazy', {
-  mounted(el, binding) {
-    console.log(el, binding)
-
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        if (isIntersecting) {
-          el.src = binding.value
-        }
-      }
-    )
-  }
-})
