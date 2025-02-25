10c6a49638cde03cb71969cf8c890b928a246735
百溪 <2963637346@qq.com>
2025-02-25 13:41:41 +0800

    panel组件插槽

---
+++ src/apis/home.js
@@ -0,0 +1,12 @@
+import http from '@/utils/http'
+
+export function getBannerAPI() {
+  return http({
+    url: 'home/banner'
+  })
+}
+export const findNewAPI = () => {
+  return http({
+    url: '/home/new'
+  })
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,33 +1,19 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+</script>
+
 <template>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  <div>我是home页</div>
-  </template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeHot />
+  <homeProduct />
+  <HomeNew title="新鲜好物"  subtitle="副标题1"/>
+  <HomePanel title="人气推荐"  subtitle="副标题2"/>
+</template>
