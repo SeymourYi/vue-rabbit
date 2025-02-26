74b3f21406e57ac19bb6e7c1f87da9bc338d2fa9
百溪 <2963637346@qq.com>
2025-02-26 16:58:50 +0800

    二级路由导航

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import LayoutView from '@/views/Layout/Index.vue'
 import LoginView from '@/views/Login/Index.vue'
 import CategoryView from '@/views/Category/index.vue'
 import HomeView from '@/views/Home/index.vue'
-
+import SubCategory from "@/views/subCategory/index.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,6 +18,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: CategoryView
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
